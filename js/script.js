7dbc8dc1340659ea0d6fd3f2ee8f5e498910ab51
madji <mdiaw13@gmail.com>
2021-10-17 21:17:07 -0400

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,31 +1,32 @@
 const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
-const imgDiv = document.querySelector(".image-container");
-const img = document.querySelector(".img");
+const quoteDiv = document.querySelector(".quote-container");
+const quote = document.querySelector("article");
 
-const getImage = async function () {
-    const res = await fetch("https://picsum.photos/v2/list?limit=100");
-    const images = await res.json();
-    //console.log(images);
-    selectRandomImage(images);
+const getQuote = async function () {
+    const res = await fetch("https://type.fit/api/quotes");
+    const quotes = await res.json();
+    console.log(quotes);
+    selectRandomQuote(quotes);
 };
 
-const selectRandomImage = function (images) {
-    const randomIndex = Math.floor(Math.random() * images.length);
+const selectRandomQuote = function (quotes) {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
     console.log(randomIndex);
-    const randomImage = images[randomIndex];
-    //console.log(randomImage);
-    displayImage(randomImage);
+    const randomQuote = quotes[randomIndex];
+    //console.log(randomquote);
+    displayQuote(randomQuote);
 };
 
-const displayImage = function (randomImage) {
-    const author = randomImage.author;
-    const imageAddress = randomImage.download_url;
+const displayQuote = function (randomQuote) {
+    console.log(randomQuote);
+    const quoteAddress = randomQuote.text;
+    quote.innerText = quoteAddress;
+    const author = randomQuote.author;
     authorSpan.innerText = author;
-    img.src = imageAddress;
-    imgDiv.classList.remove("hide");
+    quoteDiv.classList.remove("hide");
 };
 
 button.addEventListener("click", function () {
-    getImage();
+    getQuote();
 });
